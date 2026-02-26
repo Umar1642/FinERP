1e4f741302e09277d5723462f6f971f57ea3b483
nik3tR <84939453+nik3tR@users.noreply.github.com>
2026-02-26 14:53:58 +0000

    first commit

---
+++ FinERP/Models/Salary.cs
@@ -0,0 +1,10 @@
+namespace FinERP.Models
+{
+    public class Salary
+    {
+        public int Id { get; set; }
+        public string EmployeeName { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
